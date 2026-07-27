e9a0d0936026170e3f959f89f7f703ce4036c778
Thymen van Scheppingen <t.c.scheppingen@protonmail.com>
2026-07-27 11:59:30 +0200

    Updated Chadwm run.sh script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,10 @@
 xrdb merge ~/.Xresources 
 xbacklight -set 10 &
 feh --bg-fill ~/.config/chadwm/norway.jpg &
+# Modifiy screen set-up
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output HDMI-1-0 --mode 1920x1080 --pos 1920x0 --rotate normal
+
+
 xset r rate 200 50 &
 picom &
 
